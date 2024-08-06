b45cc144aac38c4b26d0d9e7c1e7a9cd90f423b1
offic <offic@DESKTOP-GLF4UNU>
2024-08-06 22:35:22 +0600

    Array_List, if you want any contains find.

--- src/main/java/com/kamrul/arraylist/ArrayList1.java
+++ src/main/java/com/kamrul/arraylist/ArrayList1.java
@@ -5,7 +5,7 @@ public class ArrayList1 {
         
         ArrayList<Integer> number = new ArrayList<>();  //----------(1)
 
-        System.out.println("arraylist size : " + number.size()); //----------(3)
+        System.out.println("arraylist size : " + number.size());
         System.out.printf("\n");
 
         //add elements to ArrayList
@@ -15,9 +15,15 @@ public class ArrayList1 {
         number.add(40);
         number.add(4, 50); //----------(2)
         
-        //replace any element 
-        number.set(3, 60); //----------(3)
-        System.out.print("after setting element = " + number);
+        //if you want 1 contains find -----------------
+        int x = number.get(1);
+        System.out.print("1 no index is in the list = " + x);
+
+        System.out.printf("\n\n");
+
+        //if you want 1 contains find -----------------
+        boolean contains = number.contains(40);
+        System.out.println("40 is in the list ? " + contains);
     }
     /*
         Some methods related to ArrayList
@@ -37,7 +43,6 @@ public class ArrayList1 {
     1নং সমীকরণে ArrayList1 এর size নির্ধারণ করে দেওয়া হয় নাই, আপনি যা elements দিবেন, তা auto add হবে।।
     2নং সমীকরণে নির্ধারণ করে দেওয়া হয়েছে, যে এই element -কে এতো নাম্বারে সেট করে দাও।।
         কোনো element কত নাম্বার স্থানে দেখতে চাও, তা নির্ধারণ করে দেওয়া যায়।।
-    3নং সমীকরণে দেখানো হয়েছে, এই element কে ৩নং element-এ replace করে দাও।।
     
     বি.দ্র.: ArrayList নামে same class create করতে পারবেন না।। যদি এখানে সমস্যা না ও করে, তবে app crush করবে।।
     
