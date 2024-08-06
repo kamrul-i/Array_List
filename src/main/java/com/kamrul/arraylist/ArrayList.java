843a6af8a968b7bdb375b35729bf65825e2bab3b
offic <offic@DESKTOP-GLF4UNU>
2024-08-06 21:30:47 +0600

    Welcome! Introduction to Array_List.

---
+++ src/main/java/com/kamrul/arraylist/ArrayList.java
@@ -0,0 +1,8 @@
+package com.kamrul.arraylist;
+
+public class ArrayList {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to ArrayList.");
+    }
+}
